2c6c1a40a4c9f51741ce09aa4be33a3b680eeaf5
Mark Broerkens <mark.broerkens@mhp.com>
2024-05-31 12:35:34 +0200

    fix: use donkey car name instead of IP

--- car_mark/sync_data.sh
+++ car_mark/sync_data.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz --delete -e ssh donkey@10.9.141.1:/home/donkey/cars/donkeycar-blue/car_mark/data/ data
+rsync -avz --delete -e ssh donkey@donkeycar-blue.local:/home/donkey/cars/donkeycar-blue/car_mark/data/ data
